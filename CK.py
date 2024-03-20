2d4ef47be482145a38889b416d3b3df49be18409
Sanghitam101 <164131140+Sanghitam101@users.noreply.github.com>
2024-03-20 21:20:51 +0800

    Create CK.py

---
+++ CK.py
@@ -0,0 +1,75 @@
+!#user/bin/python
+
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan mengetik
+        time.sleep(random.random() * 0.1)
+print ('''\33[1;39m
+╭───────────────────────────────────────────────╮
+''')
+print ('\33[1;35mN͜A͜M͜A͜ F͜A͜C͜E͜B͜O͜O͜K͜  : O͜M͜ F͜A͜R͜D͜I͜')
+print ('''\33[1;36m
+╭━━━━━━━╮
+┃　 ══　┃     YANG SELALU PAKAI SC INI
+┃███████┃     JANGAN LUPA FOLLOW TOLOL
+┃███████┃     ABIA PAKE LARI PENDO MEMANG NGANA
+┃███████┃
+┃███████┃    NGANA KIRA NYA SIKSA BEKENG INI
+┃███████┃
+┃███████┃   BRU HELE FOLLOW NYA ADA
+┃███████┃
+┃███████┃   ANAK ANJING MEMANG PUKI
+┃　 O　 ┃
+╰━━━━━━━╯
+''')
+
+
+print ('''\33[1;39m
+╰───────────────────────────────────────────────╯
+''')
+mengetik('''
+\33[1;32mPERKENALKAN NAMA SAYA FARDI DALITA
+SAYA SENGAJA MEMBUAT SITUS INI AGAR KALIAN TIDAK
+SALA MEMAHAMI (SC) TETAPI KALIAN HARUS HATI HATI
+MENGGUNAKAN (SC) KARNA PROSES KERJANYA BERJALANimport random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan mengetik
+        time.sleep(random.random() * 0.1)
+print ('''\33[1;39m
+╭───────────────────────────────────────────────╮
+''')
+print ('\33[1;35mN͜A͜M͜A͜ F͜A͜C͜E͜B͜O͜O͜K͜  : O͜M͜ F͜A͜R͜D͜I͜')
+print ('''\33[1;36m
+╭━━━━━━━╮
+┃　 ══　┃     YANG SELALU PAKAI SC INI
+┃███████┃     JANGAN LUPA FOLLOW TOLOL
+┃███████┃     ABIA PAKE LARI PENDO MEMANG NGANA
+┃███████┃
+┃███████┃    NGANA KIRA NYA SIKSA BEKENG INI
+┃███████┃
+┃███████┃   BRU HELE FOLLOW NYA ADA
+┃███████┃
+┃███████┃   ANAK ANJING MEMANG PUKI
+┃　 O　 ┃
+╰━━━━━━━╯
+''')
+
+
+print ('''\33[1;39m
+╰───────────────────────────────────────────────╯
+''')
+mengetik('''
+\33[1;32mPERKENALKAN NAMA SAYA FARDI DALITA
+SAYA SENGAJA MEMBUAT SITUS INI AGAR KALIAN TIDAK
+SALA MEMAHAMI (SC) TETAPI KALIAN HARUS HATI HATI
+MENGGUNAKAN (SC) KARNA PROSES KERJANYA BERJALAN''')
